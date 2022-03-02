b7824d8b846d89d0a24bf9cbb40fa61524e765af
gcxfd <i@rmw.link>
2022-03-02 23:07:17 +0800

    🔶

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
-use std::io::{self, Read, Write};
 use std::error::Error;
-fn main() -> Result<(), Box<Error>> {
+use std::io::{self, Read, Write};
+use std::{env, fs};
+
+fn fmt(txt: &str) -> String {
+  let value: toml::value::Value = toml::de::from_str(txt).unwrap();
+  toml::ser::to_string_pretty(&value).unwrap()
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+  let args: Vec<String> = env::args().collect();
+  if args.len() > 1 {
+    for i in &args[1..] {
+      let txt = fs::read_to_string(i)?;
+      fs::File::create(i)?.write_all(fmt(&txt).as_ref())?;
+    }
+  } else {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
-    let value: toml::value::Value = toml::de::from_str(&input)?;
-    let output = toml::ser::to_string_pretty(&value)?;
-    io::stdout().write_fmt(format_args!("{}", output))?;
-    Ok(())
+
+    io::stdout().write_fmt(format_args!("{}", fmt(&input)))?;
+  }
+
+  Ok(())
 }
